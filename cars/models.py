f9c259c184ca72524d35c4c33bbde68f9083e6ac
Romes8 <roman.zvoda@yahoo.com>
2021-12-03 02:43:16 +0100

    Migrations repaired

--- cars/models.py
+++ cars/models.py
@@ -7,18 +7,18 @@ class Car(models.Model):
     car_model = models.CharField(max_length=20)
     car_description = models.CharField(max_length=70)
     engine_type = models.CharField(max_length=20)
-    power = models.IntegerChoices
-    tourque = models.IntegerChoices
-    no_of_cylinder = models.IntegerField
-    valves_per_cylinder = models.IntegerField
+    power = models.IntegerField()
+    tourque = models.IntegerField()
+    no_of_cylinder = models.IntegerField()
+    valves_per_cylinder = models.IntegerField()
     transmission_type = models.CharField(max_length=20)
     drive_type = models.CharField(max_length=20)
     fuel_type = models.CharField(max_length=20)
     body_type = models.CharField(max_length=20)
-    seating_capacity = models.IntegerField
-    wheel_size = models.IntegerField
-    top_speed = models.IntegerField
-    acceleration_to_100 = models.DecimalField
+    seating_capacity = models.IntegerField()
+    wheel_size = models.IntegerField()
+    top_speed = models.IntegerField()
+    acceleration_to_100 = models.IntegerField()
     price = models.IntegerField()
 
 
